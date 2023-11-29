62e8d53df7b5c4a9482c2cfce81eeb5da4c955da
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-11-29 20:21:29 +0300

    Update database columns

--- db/migrate/20231128190630_create_like.rb
+++ db/migrate/20231128190630_create_like.rb
@@ -3,8 +3,6 @@ class CreateLike < ActiveRecord::Migration[7.1]
     create_table :likes do |t|
       t.references :user, null: false, foreign_key: true, index: true
       t.references :post, null: false, foreign_key: true, index: true
-      t.datetime :update_at
-      t.datetime :create_at
 
       t.timestamps
     end
