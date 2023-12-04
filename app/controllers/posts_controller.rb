ef39281ddbf4cd79518e7527f8042da5a8a2107a
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-04 16:09:57 +0300

    Add code to handle only the 4 required URLs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,5 @@
 class PostsController < ApplicationController
   def index; end
 
-  def new; end
-
-  def create; end
-
   def show; end
-
-  def edit; end
-
-  def update
-    # Controller logic to handle post updates
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,5 @@
 class UsersController < ApplicationController
+  def index; end
 
-    def index
-    end
-
-    def new
-    end
-
-    def create
-    end
-    
-    def show
-    end
-
-    def edit
-    end
-
-    def update
-    end
-    
+  def show; end
 end
