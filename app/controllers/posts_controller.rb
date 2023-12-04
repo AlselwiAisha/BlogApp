dd00ea4aeb672c0c49c9f271d552173825ddd86e
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-04 13:26:29 +0300

    Add Posts Controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def create; end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    # Controller logic to handle post updates
+  end
+end
