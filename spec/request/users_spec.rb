338ae89ac0ac514c60a1333baf18dce84f0b6692
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-05 17:31:04 +0300

    Create users_spec.rb

---
+++ spec/request/users_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    it 'Response status is correct' do
+      get '/users'
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Returns a successful response for index' do
+      get '/users'
+      expect(response).to be_successful
+    end
+
+
+    it 'Response body includes correct placeholder text' do
+      get '/users'
+      expect(response.body).to include('List of Users')
+    end
+  end
+
+  describe 'GET #show' do
+    it 'Response status is correct' do
+      get '/users/:id'
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Returns a successful response for show' do
+      get '/users/:id'
+      expect(response).to be_successful
+    end
+
+    it 'Response body includes correct placeholder text' do
+      get '/users/:id'
+      expect(response.body).to include('User data')
+    end
+  end
+end
