71d379bb132a94ed4871811ceed660e8dfbb0424
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-06 22:13:29 +0300

    Update index and show in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    @user = User.find_by(id: params[:id])
+    @posts = @user.recent_posts
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+  end 
 end
