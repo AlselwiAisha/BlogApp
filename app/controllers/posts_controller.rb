6ae3b39c3c98c2aae25d56644252be99b4bacd79
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-07 22:19:20 +0300

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,26 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+  
+  def new 
+   @user = User.find(params[:user_id])
+    @add_new_post = @user.posts.build
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(@user), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:add_new_post).permit(:title, :text)
+  end
 end
