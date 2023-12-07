f5f1c0d972904b6ca75a07bf726b4c252080145d
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-07 22:31:58 +0300

    Fix linter error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     current_user = User.find(params[:user_id])
     current_post = Post.find_by(id: params[:post_id])
-    like = Like.new(user:current_user, post: current_post)
+    like = Like.new(user: current_user, post: current_post)
     current_post.save
     like.save
     return unless like.save
