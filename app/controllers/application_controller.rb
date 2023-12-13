45188dfc6f1c86442b85b5ed5205f7393618f369
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-13 13:44:06 +0300

    Update database, app html, app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  def after_sign_out_path_for(_resource_or_scope)
+    request.referrer
+    '/users/sign_in'
   end
 
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+    end
+  end
 end
