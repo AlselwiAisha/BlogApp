203b70e15fa9e4506cb666ef71c2c1106b33a0be
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-15 07:04:02 +0300

    Fix project bugs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,8 +6,10 @@ class Ability
     if user.admin?
       can :manage, :all
     else
-      can :destroy, Post, user_id: user.id
+      can :create, Post, author_id: user.id
+      can :destroy, Post, author_id: user.id
       can :destroy, Comment, user_id: user.id
+      can :read, :all
     end
   end
 end
